b94aa3bd24e772b6eacdd1a440233011a7d86b43
Ferret56 <username@mail.ru>
2018-10-07 21:02:14 +0300

    Update solution.py(Homework2)

--- task2-checkFreq/solution.py
+++ task2-checkFreq/solution.py
@@ -1,3 +1,11 @@
 def solution(text: str) -> str:
-    # your solution here
-    return ""
+    letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']   
+    answer = 'z'
+    count = 0
+    text = text.lower()
+    for letter in text:        
+       if letter in letters:           
+           if text.count(letter) > count or (text.count(letter) == count and letter < answer) :
+               count = text.count(letter)
+               answer = letter 
+    return answer
